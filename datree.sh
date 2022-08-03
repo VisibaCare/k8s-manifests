7091863efc0141b17231e0243d1fbe964c64c81b
Martin-Andreasson <martin.andreasson@visibacare.com>
2022-08-03 13:31:35 +0200

    Added ability to publish datree policies.

--- datree.sh
+++ datree.sh
@@ -1,7 +1,18 @@
+Kustomize="kustomization.yaml"
+
 ## Publish any updates made to policies.yaml before running tests. 
 datree publish policies.yaml
 
-for d in */ ; do
-   kubectl kustomize $d | datree test - && echo Kustomize check for $d 
-done
+for d in */$Kustomize ; 
+   do 
+      kubectl kustomize $(dirname $d)"/" | datree test - && echo Kustomize check for $d 
+done  
+
+
+
+
+
+
+
+
 
