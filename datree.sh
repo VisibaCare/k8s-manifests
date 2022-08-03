031310f16638d7098342d2003d6aee00e878df79
Martin-Andreasson <martin.andreasson@visibacare.com>
2022-08-03 11:31:05 +0200

    Enabling PaC.

--- datree.sh
+++ datree.sh
@@ -1,3 +1,6 @@
+## Publish any updates made to policies.yaml before running tests. 
+datree publish policies.yaml
+
 for d in */ ; do
    kubectl kustomize $d | datree test - && echo Kustomize check for $d 
 done
