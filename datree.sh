d70806908b86d1aca3a642627ccff6dfc2701f06
Martin-Andreasson <martin.andreasson@visibacare.com>
2022-08-04 11:09:20 +0200

    Får duga så länge

--- datree.sh
+++ datree.sh
@@ -1,9 +1,11 @@
-Kustomize="kustomization.yaml"
-
+#!/bin/bash
 ## Publish any updates made to policies.yaml before running tests. 
 datree publish policies.yaml
 
-for d in */$Kustomize ; 
-   do 
-      kubectl kustomize $(dirname $d)"/" | datree test - ; echo Kustomize check for $d 
+for d in */*.yaml ; do
+      if [[ $d == */kustomization.yaml ]]
+         then kubectl kustomize $(dirname $d)"/" | datree test - ; echo Kustomize check for $d
+      elif [[ $d != */kustomization.yaml ]] 
+         then cat $d | datree test - ; echo K8S-Manifest test for $d
+      fi
 done
