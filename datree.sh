51def40a56277d9127dcad34b2e87a33e529b971
Martin-Andreasson <martin.andreasson@visibacare.com>
2022-08-15 12:04:00 +0200

    Fixed exit codes

--- datree.sh
+++ datree.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-## Publish any updates made to policies.yaml before running tests. 
-datree publish policies.yaml
 
-for d in */*.yaml ; do
-      if [[ $d == */kustomization.yaml ]]
-         then kubectl kustomize $(dirname $d)"/" | datree test - ; echo Kustomize check for $d
-      elif [[ $d != */kustomization.yaml ]] 
-         then cat $d | datree test - --ignore-missing-schemas ; echo K8S-Manifest test for $d
-      fi
-done
+path="${1:-.}"
+final_exit_code=0
+
+while read -r kustomization; do
+    dir="$(dirname "$kustomization")"
+    echo "Datree Kustomization Test: $kustomization"
+    set +e
+    datree kustomize test "$dir"
+    exitcode=$?
+    set -e
+    if [ "$exitcode" -gt "$final_exit_code" ]; then
+        final_exit_code="$exitcode"
+    fi
+done < <(find "$path" -type f -name 'kustomization.y*ml')
+
+if [ "$final_exit_code" = 0 ]; then
+    echo "Success"
+else
+    echo "Violations found, returning exit code $final_exit_code"
+fi
+exit "$final_exit_code"
+
+#done < <(find ./* -type f \( -iname '*.y*ml' ! -iname 'kustomization.yaml' \) )
